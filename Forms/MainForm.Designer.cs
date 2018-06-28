45a9afac9f8d7396697a79f0b87520a1a4b9c8be
Thomas Hammer <sparklez@detolly.no>
2018-06-28 05:54:55 +0200

    Tab Indexing

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -73,7 +73,7 @@
             this.textBox1.Location = new System.Drawing.Point(121, 151);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(160, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.TabIndex = 8;
             this.textBox1.Text = "forsen";
             // 
             // button1
@@ -81,7 +81,7 @@
             this.button1.Location = new System.Drawing.Point(283, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 9;
             this.button1.Text = "Open Chat";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -95,7 +95,7 @@
             this.button2.Location = new System.Drawing.Point(283, 123);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
+            this.button2.TabIndex = 7;
             this.button2.Text = "Select Font";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -139,7 +139,7 @@
             0});
             this.Emotespacing.Name = "Emotespacing";
             this.Emotespacing.Size = new System.Drawing.Size(160, 20);
-            this.Emotespacing.TabIndex = 7;
+            this.Emotespacing.TabIndex = 6;
             this.Emotespacing.ValueChanged += new System.EventHandler(this.Emotespacing_ValueChanged);
             // 
             // label5
@@ -157,7 +157,7 @@
             this.AnimationsCheckBox.Location = new System.Drawing.Point(121, 73);
             this.AnimationsCheckBox.Name = "AnimationsCheckBox";
             this.AnimationsCheckBox.Size = new System.Drawing.Size(15, 14);
-            this.AnimationsCheckBox.TabIndex = 9;
+            this.AnimationsCheckBox.TabIndex = 4;
             this.AnimationsCheckBox.UseVisualStyleBackColor = true;
             this.AnimationsCheckBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
@@ -175,7 +175,7 @@
             this.BackgroundColorBox.Location = new System.Drawing.Point(121, 42);
             this.BackgroundColorBox.Name = "BackgroundColorBox";
             this.BackgroundColorBox.Size = new System.Drawing.Size(160, 20);
-            this.BackgroundColorBox.TabIndex = 11;
+            this.BackgroundColorBox.TabIndex = 2;
             this.BackgroundColorBox.Text = "#111111";
             // 
             // ForegroundColorBox
@@ -183,7 +183,7 @@
             this.ForegroundColorBox.Location = new System.Drawing.Point(121, 16);
             this.ForegroundColorBox.Name = "ForegroundColorBox";
             this.ForegroundColorBox.Size = new System.Drawing.Size(160, 20);
-            this.ForegroundColorBox.TabIndex = 12;
+            this.ForegroundColorBox.TabIndex = 0;
             this.ForegroundColorBox.Text = "#FFFFFF";
             // 
             // label7
@@ -200,7 +200,7 @@
             this.button3.Location = new System.Drawing.Point(283, 40);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 14;
+            this.button3.TabIndex = 3;
             this.button3.Text = "Pick Color";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -210,7 +210,7 @@
             this.button4.Location = new System.Drawing.Point(283, 14);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 15;
+            this.button4.TabIndex = 1;
             this.button4.Text = "Pick Color";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
@@ -221,7 +221,7 @@
             this.radioButton1.Location = new System.Drawing.Point(378, 17);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(54, 17);
-            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabIndex = 10;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Empty";
             this.radioButton1.UseVisualStyleBackColor = true;
@@ -233,7 +233,7 @@
             this.radioButton2.Location = new System.Drawing.Point(378, 43);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(54, 17);
-            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabIndex = 11;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Empty";
             this.radioButton2.UseVisualStyleBackColor = true;
@@ -245,7 +245,7 @@
             this.radioButton3.Location = new System.Drawing.Point(378, 71);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(54, 17);
-            this.radioButton3.TabIndex = 18;
+            this.radioButton3.TabIndex = 12;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Empty";
             this.radioButton3.UseVisualStyleBackColor = true;
@@ -257,7 +257,7 @@
             this.radioButton4.Location = new System.Drawing.Point(378, 98);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(54, 17);
-            this.radioButton4.TabIndex = 19;
+            this.radioButton4.TabIndex = 13;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Empty";
             this.radioButton4.UseVisualStyleBackColor = true;
@@ -269,7 +269,7 @@
             this.radioButton5.Location = new System.Drawing.Point(378, 126);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(54, 17);
-            this.radioButton5.TabIndex = 20;
+            this.radioButton5.TabIndex = 14;
             this.radioButton5.TabStop = true;
             this.radioButton5.Text = "Empty";
             this.radioButton5.UseVisualStyleBackColor = true;
@@ -281,7 +281,7 @@
             this.radioButton6.Location = new System.Drawing.Point(378, 152);
             this.radioButton6.Name = "radioButton6";
             this.radioButton6.Size = new System.Drawing.Size(54, 17);
-            this.radioButton6.TabIndex = 21;
+            this.radioButton6.TabIndex = 15;
             this.radioButton6.TabStop = true;
             this.radioButton6.Text = "Empty";
             this.radioButton6.UseVisualStyleBackColor = true;
@@ -302,7 +302,7 @@
             this.button5.Location = new System.Drawing.Point(283, 68);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 23;
+            this.button5.TabIndex = 5;
             this.button5.Text = "Settings";
             this.button5.UseVisualStyleBackColor = true;
             // 
