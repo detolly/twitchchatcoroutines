e67d7e5e29abe95cfe8ba54c9fea49d521f331e8
Thomas Hammer <sparklez@detolly.no>
2018-07-05 20:50:35 +0200

    New branch for changing labels to "TextBox"es

--- Classes and Structs/TwitchLabel.cs
+++ Classes and Structs/TwitchLabel.cs
@@ -4,7 +4,7 @@ using System.Windows.Forms;
 
 namespace TwitchChatCoroutines.ClassesAndStructs
 {
-    class TwitchLabel : Label
+    class TwitchLabel : TextBox
     {
         protected override void OnHandleCreated(EventArgs e)
         {
